8629cbe7dc9133848b62628e30b4164b37455702
shyameshjain178 <shyamesh.jain2025@glbajajgroup.org>
2026-06-15 18:49:50 +0530

    Add files via upload

---
+++ DAY7/Q25.c
@@ -0,0 +1,16 @@
+//Recursive Factorial
+
+#include<stdio.h>
+int fact(int n)
+{
+    if(n==0||n==1)
+        return 1;
+    return n*fact(n-1);
+}
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    printf("%d",fact(n));
+    return 0;
+}
