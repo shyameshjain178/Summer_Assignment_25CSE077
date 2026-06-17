d0c68990b3e053c35866991ac01d06c33adb2a44
shyameshjain178 <shyamesh.jain2025@glbajajgroup.org>
2026-06-17 20:21:22 +0530

    Add files via upload

---
+++ DAY9/Q35.c
@@ -0,0 +1,15 @@
+//repeated character
+
+#include<stdio.h>
+int main(){
+int i,j,n;
+char ch='A';
+scanf("%d",&n);
+for(i=1;i<=n;i++){
+for(j=1;j<=i;j++)
+printf("%c",ch);
+printf("\n");
+ch++;
+}
+return 0;
+}
