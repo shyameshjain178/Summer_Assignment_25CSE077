db9122f3e294444962b803721445fcfbfed8de67
shyameshjain178 <shyamesh.jain2025@glbajajgroup.org>
2026-06-18 19:42:01 +0530

    Add files via upload

---
+++ DAY10/Q40.c
@@ -0,0 +1,18 @@
+//character pyramid
+
+#include<stdio.h>
+int main(){
+int i,j,n;
+printf("Enter number of rows: ");
+scanf("%d",&n);
+for(i=1;i<=n;i++){
+for(j=1;j<=n-i;j++)
+printf(" ");
+for(j=0;j<i;j++)
+printf("%c",'A'+j);
+for(j=i-2;j>=0;j--)
+printf("%c",'A'+j);
+printf("\n");
+}
+return 0;
+}
