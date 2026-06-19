c96c7c0250809eeacc655ad6aa192e7915907a08
shyameshjain178 <shyamesh.jain2025@glbajajgroup.org>
2026-06-19 21:50:29 +0530

    Add files via upload

---
+++ DAY11/Q42.c
@@ -0,0 +1,17 @@
+//find maximum
+
+#include<stdio.h>
+int max(int a,int b)
+{
+if(a>b)
+return a;
+else
+return b;
+}
+int main()
+{
+int a,b;
+scanf("%d%d",&a,&b);
+printf("%d",max(a,b));
+return 0;
+}
